5e8b70a6a58611121e6f3cf27ef7bc8bf4445e46
Paul Rouget <me@paulrouget.com>
2019-11-21 06:14:03 +0100

    run test-tidy after build

--- etc/taskcluster/decision_task.py
+++ etc/taskcluster/decision_task.py
@@ -432,9 +432,9 @@ def windows_uwp_x64():
         windows_build_task("UWP dev build", package=False)
         .with_treeherder("Windows x64")
         .with_script(
-            "python mach test-tidy --force-cpp --no-wpt",
             "python mach build --dev --target=x86_64-uwp-windows-msvc",
             "python mach package --dev --target=x86_64-uwp-windows-msvc --uwp=x64",
+            "python mach test-tidy --force-cpp --no-wpt",
         )
         .with_artifacts(appx_artifact(debug=True))
         .find_or_create("build.windows_uwp_x64_dev." + CONFIG.task_id())
